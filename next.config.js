2862439e71e7419845e9725c9cbff6e3ddd9d6d2
2Revol2 <maksimbaluta02@gmail.com>
2026-03-22 16:31:33 +0100

    remove upload, local endpoints and add next.js rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { config } = require("./app/config.ts");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+
+  async rewrites() {
+    return [
+      {
+        source: "/upload/:path*",
+        destination: `${config.SOURCE_WEBSITE}/upload/:path*`,
+      },
+      {
+        source: "/local/templates/:path*",
+        destination: `${config.SOURCE_WEBSITE}/local/templates/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
