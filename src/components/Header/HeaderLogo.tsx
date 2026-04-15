ec2010f82837bc4bcb6dc7b054cac1e9b773a869
Mikita <usesa@yandex.com>
2026-04-15 08:10:14 +0200

    [#102] Fix logo image warning

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -7,12 +7,14 @@ import { useIsMobile } from "@/src/hooks/useIsMobile";
 export const HeaderLogo = () => {
   const { isMobile } = useIsMobile();
 
-  const logo = <Image src={"/ab-market/logo.jpg"} alt={"logo"} width={162} height={70} />;
+  const logo = (
+    <Image src="/ab-market/logo.jpg" alt="logo" width={162} height={70} preload style={{ height: "auto" }} />
+  );
 
   return isMobile ? (
     logo
   ) : (
-    <Link href={"/"} className={"border-b-0!"}>
+    <Link href="/" className="border-b-0!">
       {logo}
     </Link>
   );
