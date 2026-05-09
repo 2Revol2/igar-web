3634c7efe5e90311165c12c5886f77e37f1b2900
2Revol2 <maksimbaluta02@gmail.com>
2026-05-09 14:25:29 +0200

    Refactor RUB to BYN rate fetching with local cache

--- src/lib/client/rub-to-byn-rate.ts
+++ src/lib/client/rub-to-byn-rate.ts
@@ -1,10 +1,22 @@
-export async function getRubToBynRate(): Promise<number> {
-  const primaryUrl = "https://api.nbrb.by/exrates/rates/RUB?parammode=2";
+import { join } from "path";
+import { readFile, writeFile } from "fs/promises";
+import { existsSync } from "node:fs";
 
-  const fallbackUrl = "https://latest.currency-api.pages.dev/v1/currencies/rub.json";
+const CACHE_DIR = join(process.cwd(), "cache");
+const RATE_CACHE_FILE = join(CACHE_DIR, "rub-byn-rate.json");
+
+interface RubRate {
+  Cur_ID: number;
+  Cur_Abbreviation: string;
+  Cur_Scale: number;
+  Cur_OfficialRate: number;
+}
+
+export async function getRubToBynRate(): Promise<number> {
+  const url = "https://api.nbrb.by/exrates/rates/RUB?parammode=2";
 
   try {
-    const res = await fetch(primaryUrl, {
+    const res = await fetch(url, {
       next: { revalidate: 60 * 60 }, // 1 hour
     });
 
@@ -12,37 +24,33 @@ export async function getRubToBynRate(): Promise<number> {
       throw new Error(`NBRB request failed: ${res.status}`);
     }
 
-    const data: {
-      Cur_ID: number;
-      Cur_Abbreviation: string;
-      Cur_Scale: number;
-      Cur_OfficialRate: number;
-    } = await res.json();
+    const data: RubRate = await res.json();
 
     if (!data.Cur_OfficialRate || !data.Cur_Scale) {
       throw new Error("Invalid NBRB response");
     }
 
-    // Например, если 100 RUB = 3.7003 BYN,
-    // то 1 RUB = 3.7003 / 100
+    await writeFile(RATE_CACHE_FILE, JSON.stringify(data), "utf-8");
+
     return data.Cur_OfficialRate / data.Cur_Scale;
   } catch (error) {
-    console.warn("Primary API failed, using fallback...", error);
+    console.error("[RUB/BYN] API failed", error);
+    try {
+      if (!existsSync(RATE_CACHE_FILE)) {
+        throw new Error("Cache does not exist");
+      }
 
-    const fallbackRes = await fetch(fallbackUrl, {
-      next: { revalidate: 60 * 60 },
-    });
+      const dataRaw = await readFile(RATE_CACHE_FILE, "utf-8");
+      const data = JSON.parse(dataRaw) as RubRate;
 
-    if (!fallbackRes.ok) {
-      throw new Error(`Fallback request failed: ${fallbackRes.status}`);
+      if (!data.Cur_OfficialRate || !data.Cur_Scale) {
+        throw new Error("Invalid cache");
+      }
+
+      return data.Cur_OfficialRate / data.Cur_Scale;
+    } catch (cacheError) {
+      console.error("[RUB/BYN] Cache fallback failed", cacheError);
+      throw new Error("Unable to get RUB/BYN rate");
     }
-
-    const fallbackData: {
-      rub: {
-        byn: number;
-      };
-    } = await fallbackRes.json();
-
-    return fallbackData.rub.byn;
   }
 }
